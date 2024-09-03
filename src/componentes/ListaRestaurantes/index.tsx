dead82e84bb19da896facb34b5cf276e13e2d48c
VitorCapovillaMast <vitor.capovilla@mastsolucoes.com>
2024-09-03 13:02:59 -0300

    Paginação da lista de restaurantes

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -3,24 +3,39 @@ import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import { useEffect, useState } from 'react';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [PaginaProximo, setPaginaProximo] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
     .then(resposta => {
       setRestaurantes(resposta.data.results)
+      setPaginaProximo(resposta.data.next)
     })
     .catch(error => {
       console.log(error);
     })
   }, [])
 
+  const verProxima = () => {
+    axios.get<IPaginacao<IRestaurante>>(PaginaProximo)
+    .then(resposta => {
+      setRestaurantes([...restaurantes, ...resposta.data.results])
+      setPaginaProximo(resposta.data.next)
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {PaginaProximo && <button onClick={verProxima}>Próxima</button>}
   </section>)
 }
 
